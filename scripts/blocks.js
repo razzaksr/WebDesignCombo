465986c61f29ad050ce0b92705efc155085b74dd
razzaksr <razzaksr@gmail.com>
2022-02-16 14:14:52 +0530

    functions

---
+++ scripts/blocks.js
@@ -0,0 +1,26 @@
+function vj()
+{
+    alert("Vijay simply developer")
+}
+function vijay(yet)
+{
+    alert(yet+" found")
+}
+function manoj(hai,hello,what)
+{
+    alert(hai+" "+hello+" "+what)
+}
+function raj()
+{
+    return "Hamsaraj consultant in digital marketting"
+}
+// function hybrid(queen,king)
+// {
+//     return queen*king
+// }
+const hybrid=(queen,king)=>{
+    return queen*king
+}
+var uma=()=>{
+    alert("Fat arrow function")
+}
