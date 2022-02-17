68fc59eccda636673fa19c044c3dae2986b45b96
razzaksr <razzaksr@gmail.com>
2022-02-17 14:20:47 +0530

    interactive table via array

--- scripts/forarray.js
+++ scripts/forarray.js
@@ -9,5 +9,12 @@ const inserting=()=>{
 }
 
 const view=()=>{
-    document.write("<table align='center' style='padding: 10px 10px 10px 10px; border-radius: 10px;box-shadow: 5px 5px 5px blue;'><thead><tr><th>Elements</th></tr></thead><tbody>"+delta.map((data)=>{document.write("<tr><td>"+data+"</td></tr>")})+"</tbody></table>")
+    var hi=""
+    delta.map((data)=>
+    {   
+        hi+="<tr><td>"+data+"</td></tr>"
+    })
+    var t=document.getElementById("present")
+    //document.write("<table align='center' style='padding: 10px 10px 10px 10px; border-radius: 10px;box-shadow: 5px 5px 5px blue;'><thead><tr><th>Elements</th></tr></thead><tbody>"+delta.map((data)=>{document.write("<tr><td>"+data+"</td></tr>")})+"</tbody></table>")
+    t.innerHTML="<table align='center' style='padding: 10px 10px 10px 10px; border-radius: 10px;box-shadow: 5px 5px 5px blue;'><thead><tr><th>Elements</th></tr></thead><tbody>"+hi+"</tbody></table>"
 }
